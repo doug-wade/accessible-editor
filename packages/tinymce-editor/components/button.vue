2c715aaa9cbafe7737bafe7486652c05de4b478f
Doug Wade <doug.wade@skilljar.com>
2023-08-16 19:12:08 -0700

    checkpoint

---
+++ packages/tinymce-editor/components/button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="btn btn-primary"></button>
+</template>
+
+<script>
+export default {
+  name: "ae-button",
+};
+</script>
+
+<style scoped>
+.btn {
+  border-radius: 0.25rem;
+}
+
+.btn-primary {
+  background-color: rebeccapurple;
+}
+</style>
---
+++ packages/tinymce-editor/components/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ae-tinymce></ae-tinymce>
+    <ae-button @click="{clickHandler}">Check</ae-button>
+    <ae-gutter></ae-gutton>
+  </div>
+</template>
+
+<script lang="ts">
+import TinyMCE from './tinymce';
+import Button from './button';
+import Gutter from './gutter';
+
+export default {
+  name: 'accessible-editor',
+  setup() {
+    const clickHandler = async () => {
+      const result = fetch('http://localhost:3000/');
+      const json = result.json();
+      
+      console.log(json);
+    }
+    return {
+      clickHandler
+    }
+  }
+}
+</script>
